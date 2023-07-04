99ec03d23b80460d6252844211bc93cf0e7fe48d
babaolanqiu <babaolanqiu@gmail.com>
2023-07-04 22:23:52 +0800

    ✨ Cleandata QC

--- codes/02.cleandata_QC.sh
+++ codes/02.cleandata_QC.sh
@@ -1,18 +1,22 @@
 #bin/bash
+source ~/.envrc
+echo $(pwd)
 # get the fastqc results for all fq.gz files and integrated report by multiqc
-indir=0.cleandata
-outdir=$(basename $0 .sh) # use the basename of current .sh file to create a outdir
+indir=./data/02.CleanData
+outdir=./results/02.CleanData_QC
 mkdir -p $outdir          # make dir at current working directionary
-export PATH="/home/guozhonghao/.conda/envs/fastqc/bin:$PATH"
+# export PATH="/home/guozhonghao/.conda/envs/fastqc/bin:$PATH"
 # for fq in $(find $indir -type f -name "*.fq.gz"); do
 # 	fastqc -o $outdir $fq &
 # done
 # convert the for loop to a function and use parallel to run the function
 getFastqc() {
 	fq=$1
-	/home/guozhonghao/.conda/envs/fastqc/bin/fastqc -o ./1.cleandata_QC $fq
+outdir=./results/02.CleanData_QC
+	# mkdir -p $outdir/$(dirname $fq)
+	fastqc -o $outdir $fq
 }
 export -f getFastqc
-parallel getFastqc :::: <(find $indir -type f -name "*.fq.gz")
+parallel --progress --keep-order --line-buffer getFastqc :::: <(find $indir -type f -name "*.fq.gz")
 # use multiqc to combine the reports from the folder $outdir, name the integrated report file with outdir name, put the output report file into $outdir
-multiqc -i $outdir -o $outdir $outdir
+/data0/apps/anaconda3/bin/multiqc -i $outdir -o $outdir $outdir
